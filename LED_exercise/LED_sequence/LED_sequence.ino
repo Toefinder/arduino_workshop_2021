95da795515cf7b635982725f985b1a0265852a97
Toefinder <lequanganhok@gmail.com>
2021-09-05 23:25:38 +0800

    Edit code for exercise

--- LED_exercise/LED_sequence/LED_sequence.ino
+++ LED_exercise/LED_sequence/LED_sequence.ino
@@ -1,27 +1,4 @@
-//Flowing LED Lights
-
-/*
-  Eight LEDs will light up one by one from left to right, and then go out one by
-  one from right to left.
-
-  After
-  that, the LEDs will light up one by one from right to left, and then go out one
-  by one from left to right.
-
-  This
-  process will repeat indefinitely.*
-
-  /Email:info@primerobotics.in
-
-  //Website:www.primerobotics.in
-
-  /**************************************/
-
-const int lowestPin = 2;//the lowest one attach to
-
-const int highestPin = 9;//the highest one attach to
-
-byte pinArray[] = {2, 3, 4, 5, 6, 7, 8, 10};
+byte pinArray[] = {2, 3, 6, 8, 10, 12};
 int pinArraySize = sizeof(pinArray);
 int thisPin;
 char incomingByte = 'n'; // for incoming serial data
@@ -54,7 +31,7 @@ void loop()
     Serial.println(incomingByte);
   }
   if (incomingByte == 'r') {
-    //turn the led on from lowest to the highest and back
+    //turn the led on from highest to the lowest
     for (int pinIndex = pinArraySize-1; pinIndex >= 0; pinIndex--)
     {
       thisPin = pinArray[pinIndex];
@@ -70,7 +47,7 @@ void loop()
       delay(100);//wait for 100 ms
     }
   } else {
-  //turn the led on from lowest to the highest and back
+  //turn the led on from lowest to the highest
     for (int pinIndex = 0; pinIndex < pinArraySize; pinIndex++)
     {
       thisPin = pinArray[pinIndex];
